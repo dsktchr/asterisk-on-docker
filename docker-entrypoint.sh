4ffdcfef48142dd0fe2dedcad6a74a18f90d24fe
Daisuke Tachihara <tachihara.d@everbank.co.jp>
2022-04-25 13:23:32 +0900

    fix: Asterisk起動コマンドの修正

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,4 @@
 
 echo "Starting Asterisk Server..."
 # Asterisk execute as asterisk user and asterisk group
-/usr/sbin/asterisk -U asterisk -G asterisk -cvvvvv
+/usr/sbin/asterisk -U asterisk -G asterisk 
